4d7b8daf57d96b5d10986efc40e70e9daf23e8db
EvGen Gal <genwolk1@gmail.com>
2022-07-28 23:06:25 +0300

    R|A. RTG. CSSTransition всё. нач SwitchTransition

--- README.md
+++ README.md
@@ -44,6 +44,7 @@ npm i react-router-dom react-spring
 #### Подкл. необходимые importы.
 
 <p>(Хук и аним из spring. Библ для динам. маршрутов, библ.(с v6 Switch заменен на Routes), комп., хук(возвращ. текущий URL), ссылки из router)</p>
+
 ```js
 import { useTransition, animated } from "react-spring";
 import {
@@ -55,18 +56,15 @@ import {
 } from "react-router-dom";
 ```
 
-- - - Хук и аним из spring.
-- `*` - Хук и аним из spring.
-- `-` - Хук и аним из spring.
-- **[`Хук и аним из spring`]()**
-- **[Хук и аним из spring]()**
-<p>Хук и аним из spring.</p>
-<p>Библ для динам. маршрутов, библ.(с v6 Switch заменен на Routes), комп., хук(возвращ. текущий URL), ссылки из router</p>
-<br/>
+<!-- - - - Хук и аним из spring. - круг заполн, круг пуст, квадр -->
+<!-- - `*` - Хук и аним из spring. - круг заполн, звёзд выдел, тире -->
+<!-- - `-` - Хук и аним из spring. - круг заполн, тире выдел, тире -->
+<!-- - **[`Хук и аним из spring`]()** - выделен -->
+<!-- - **[Хук и аним из spring]()** - выделен и жирный -->
+<!-- <br/> -->
 
 #### Структура кода.
 
-```
 - перем. `location` достаем из хука useLocation - `const location = useLocation();`. Объект логакаци необходим
 - перем. `transition` созд. из хука useTransition - `const transitions = useTransition(location, { style });`.
   <br/>Получаем объ. локации (уник. для каждой стр.), анимацию(в {} пропис. стили), (устаревшее - 3им параметром передаём key - передаём location.pathname key (уник ключ для каждой стр))
@@ -78,35 +76,22 @@ import {
   <br/>Старый код - `<Route exact path="/" component={HomePage} />`
   <br/>Новый код - `<Route exact path="/" element={<HomePage />} />`
 - Для исключения "прыжка от невидимого соседа", можно на блок до transition повесить `position: relative`, и м/у `animated` и `Routes` завести блок с `position: absolute`
-  <br/>
+  <!-- <br/> -->
+  <hr>
 
 - Проект `React SPA` перемещён в App.jsx. На его основе сделать `<Header/>` и ссылки на другие проекты.
-```
+  <!-- <br/> -->
+  <hr>
 
-- перем. `location` достаем из хука useLocation - `const location = useLocation();`. Объект логакаци необходим
-- перем. `transition` созд. из хука useTransition - `const transitions = useTransition(location, { style });`.
-  <br/>Получаем объ. локации (уник. для каждой стр.), анимацию(в {} пропис. стили), (устаревшее - 3им параметром передаём key - передаём location.pathname key (уник ключ для каждой стр))
-- устр. - (перем transitions вернет массив хука useTransition, перебор mapом до - текущих эл(item), props(стили прописаны в {...}), и уник ключ.)
-- В обёртку `transition`, заворачиваем КОД, с передачей парам. - `{transitions((props, item) => ( КОД )}`
-- В обёртку `animated`, КОД, с props - `<animated.div style={props}> КОД </animated.div>`
-- В обёртку `Routes`(ранее Switch), все `Route`, с item - `<Routes location={item}> КОД </Routes>`
-- Cоздать неск стр., для кажд созд свой `Route`, поместить их в опред структуру со свичом.
-  <br/>Старый код - `<Route exact path="/" component={HomePage} />`
-  <br/>Новый код - `<Route exact path="/" element={<HomePage />} />`
-- Для исключения "прыжка от невидимого соседа", можно на блок до transition повесить `position: relative`, и м/у `animated` и `Routes` завести блок с `position: absolute`
-<br/>
-<hr>
-- Проект `React SPA` перемещён в App.jsx. На его основе сделать `<Header/>` и ссылки на другие проекты.
-<br/>
-<hr>
 - По YT видео [Анимация перехода между страницами в React SPA с react-spring](https://www.youtube.com/watch?v=BSmXK6z0h3s)
+
   <br/>
   <br/>
   <br/>
   ...
 
 <h3 align="center">Анимации в React</h3>
-<p align="center">React Transition Group</p>
+<p align="center">(React Transition Group - Компоненты компоненты с этапами/кл. входа/выхода стиля transition)</p>
 
 #### Установка библ (компоненты с этапами/кл. входа/перехода/выхода).
 
@@ -122,6 +107,6 @@ import { Transition } from "react-transition-group";
 
 #### Структура кода.
 
-Transition - Подробнее - https://reactcommunity.org/react-transition-group/transition
-
+- `Transition` - Подробнее - https://reactcommunity.org/react-transition-group/transition
+- `CSSTransition` - Подробнее - https://reactcommunity.org/react-transition-group/css-transition
 - По YT видео [Анимации в React](https://youtu.be/7MMRcMutqpI?list=PL6DxKON1uLOH0vVpdrZhUAVpHerNbUxfF)
--- src/project/ReactTransitionGroup/ReactTransitionGroup.jsx
+++ src/project/ReactTransitionGroup/ReactTransitionGroup.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Transition } from "react-transition-group";
+import {
+  Transition,
+  CSSTransition,
+  SwitchTransition,
+} from "react-transition-group";
+// доп из react-bootstrap для примеров Доков ReactTransitionGroup
+import { Container, Button, Alert, Form } from "react-bootstrap";
 
-function ReactTransitionGroup() {
+// Transition Компонент из видео
+function TransitionComp() {
   // нов. сост. 1ый эл. - сост, 2ой - fn измен. сост. По умолч. false
   const [loaderVisible, setloaderVisible] = useState(false);
 
@@ -13,16 +20,17 @@ function ReactTransitionGroup() {
 
   return (
     <>
-      <div className="ReactTransitionGroup">
+      <div className="TransitionComp">
         {/* слушат. событ. - вызов fn и меняет сост на противоположное */}
+        <h5>Transition</h5>
         <button onClick={() => setloaderVisible(!loaderVisible)}>
           {/* динамич обновл текст */}
           {loaderVisible ? "hide" : "show"}
         </button>
         <div className="wrap">
-          {/* // до Transition. условие добавл/удал блока .cicrle
-          {loaderVisible && <div className="cicrle rotate" />} */}
-          {/* Transition обёрт. аним-ый блок. Подробнее - https://reactcommunity.org/react-transition-group/transition */}
+          {/* // до Transition. условие добавл/удал блока .circle
+        {loaderVisible && <div className="circle rotate" />} */}
+          {/* Transition оборачивает аним-ый блок. Подробнее - https://reactcommunity.org/react-transition-group/transition */}
           {/* props. in - вкл аним., timeout - время аним., mountOnEnter - монтаж, unmountOnExit - демонтаж, onEnter - слуш.событ(6 шт, можно использ.) */}
           <Transition
             in={loaderVisible}
@@ -36,13 +44,214 @@ function ReactTransitionGroup() {
             onExiting={() => console.log("onExiting")}
             onExited={() => console.log("onExited")}
           >
-            {/* Transition нужна fn. Приним. state и возвращ. аним-ый Comp. В класс + state(4 cl в цикле - вход/введено/выход/вышел) */}
-            {(state) => <div className={`cicrle ${state}`} />}
+            {/* Transition нужна fn, оборач в {}. Приним. state и возвращ. аним-ый Comp. В класс + state(4 cl в цикле - вход/введено/выход/вышел) */}
+            {(state) => <div className={`circle ${state}`} />}
           </Transition>
         </div>
       </div>
-      <div></div>
     </>
+  );
+}
+
+// CSSTransition Компонент + Доки
+function CSSTransitionComp() {
+  // из видео.нов. сост. 1ый эл. - сост, 2ой - fn измен. сост. По умолч. false
+  const [loaderVisible, setloaderVisible] = useState(false);
+  // из доков
+  const [showButton, setShowButton] = useState(true);
+  const [showMessage, setShowMessage] = useState(false);
+  // перекл м/у видео и док
+  const [showVar, setshowVar] = useState(true);
+
+  // симуляция асинх логики (тип загрузка данных для выхода loadera). 1ый timeuot покаж loader ч/з 1с, 2ой ч/з 5с. скроет
+  useEffect(() => {
+    setTimeout(() => setloaderVisible(true), 1000);
+    setTimeout(() => setloaderVisible(false), 3000);
+  }, []);
+
+  return (
+    <>
+      <div className="CSSTransitionComp">
+        {/* слушат. событ. - вызов fn и меняет сост на противоположное */}
+        <h5>CSSTransition</h5>
+        <button onClick={() => setshowVar(!showVar)}>
+          {/* динамич обновл текст */}
+          {showVar ? "вариант Видео" : "вариант Доки"}
+        </button>
+        {showVar && (
+          <button onClick={() => setloaderVisible(!loaderVisible)}>
+            {/* динамич обновл текст */}
+            {loaderVisible ? "hide" : "show"}
+          </button>
+        )}
+        <div className="wrap">
+          {/* CSSTransition оборачивает аним-ый блок. Наследник Transition расширеный. Подробнее - https://reactcommunity.org/react-transition-group/css-transition */}
+          {/* props. in - вкл аним., timeout - время аним., mountOnEnter - монтаж, unmountOnExit - демонтаж, onEnter - слуш.событ(6 шт, можно использ.) */}
+          {showVar ? (
+            <CSSTransition
+              in={loaderVisible}
+              timeout={500}
+              mountOnEnter
+              unmountOnExit
+              // в classNames можно предать объ со своими назв. стиля. Для уже существ .cl аним, указ их здесь.
+              // classNames="circle"
+              classNames={{
+                enterActive: "circle-show",
+                enterDone: "circle-rotate",
+                exitActive: "circle-hide",
+              }}
+            >
+              {/* fn не нужна. созд loader напрямую */}
+              <div className="circle"></div>
+            </CSSTransition>
+          ) : (
+            <Container style={{ margin: "0" }}>
+              {showButton && (
+                <Button onClick={() => setShowMessage(true)} size="lg">
+                  Show Message
+                </Button>
+              )}
+              <CSSTransition
+                in={showMessage}
+                timeout={300}
+                classNames="alert"
+                unmountOnExit
+                onEnter={() => setShowButton(false)}
+                onExited={() => setShowButton(true)}
+              >
+                <Alert
+                  variant="primary"
+                  dismissible
+                  onClose={() => setShowMessage(false)}
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "stretch",
+                  }}
+                >
+                  <Alert.Heading>Аним смс</Alert.Heading>
+                  <p>Это предупр</p>
+                  <Button onClick={() => setShowMessage(false)}>Close</Button>
+                </Alert>
+              </CSSTransition>
+            </Container>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+// SwitchTransitionComp Компонент + Доки
+function SwitchTransitionComp() {
+  // из видео.нов. сост. 1ый эл. - сост, 2ой - fn измен. сост. По умолч. false
+  const [loaderVisible, setloaderVisible] = useState(false);
+  // из доков
+  const [mode, setMode] = React.useState("out-in");
+  const [state, setState] = React.useState(true);
+  // перекл м/у видео и док
+  const [showVar, setshowVar] = useState(true);
+
+  // симуляция асинх логики (тип загрузка данных для выхода loadera). 1ый timeuot покаж loader ч/з 1с, 2ой ч/з 5с. скроет
+  useEffect(() => {
+    setTimeout(() => setloaderVisible(true), 1000);
+    setTimeout(() => setloaderVisible(false), 3000);
+  }, []);
+
+  const modes = ["out-in", "in-out"];
+
+  return (
+    <>
+      <div className="SwitchTransitionComp">
+        {/* слушат. событ. - вызов fn и меняет сост на противоположное */}
+        <h5>SwitchTransition</h5>
+        <button onClick={() => setshowVar(!showVar)}>
+          {/* динамич обновл текст */}
+          {showVar ? "вариант Видео" : "вариант Доки"}
+        </button>
+        {showVar && (
+          <button onClick={() => setloaderVisible(!loaderVisible)}>
+            {/* динамич обновл текст */}
+            {loaderVisible ? "hide" : "show"}
+          </button>
+        )}
+        <div
+          className="wrap"
+          // style={{showVar ? (display: "flex"): (display: "block")}}
+          style={{ display: showVar === true ? "flex" : "block" }}
+        >
+          {/* CSSTransition оборачивает аним-ый блок. Наследник Transition расширеный. Подробнее - https://reactcommunity.org/react-transition-group/css-transition */}
+          {/* props. in - вкл аним., timeout - время аним., mountOnEnter - монтаж, unmountOnExit - демонтаж, onEnter - слуш.событ(6 шт, можно использ.) */}
+          {showVar ? (
+            <CSSTransition
+              in={loaderVisible}
+              timeout={500}
+              mountOnEnter
+              unmountOnExit
+              // в classNames можно предать объ со своими назв. стиля. Для уже существ .cl аним, указ их здесь.
+              // classNames="circle"
+              classNames={{
+                enterActive: "circle-show",
+                enterDone: "circle-rotate",
+                exitActive: "circle-hide",
+              }}
+            >
+              {/* fn не нужна. созд loader напрямую */}
+              <div className="circle"></div>
+            </CSSTransition>
+          ) : (
+            <>
+              <div className="label">Mode:</div>
+              <div className="modes">
+                {modes.map((m) => (
+                  <Form.Check
+                    key={m}
+                    custom
+                    inline
+                    label={m}
+                    id={`mode=msContentScript${m}`}
+                    type="radio"
+                    name="mode"
+                    checked={mode === m}
+                    value={m}
+                    onChange={(event) => {
+                      setMode(event.target.value);
+                    }}
+                  />
+                ))}
+              </div>
+              <div className="main">
+                <SwitchTransition mode={mode}>
+                  <CSSTransition
+                    key={state}
+                    addEndListener={(node, done) => {
+                      node.addEventListener("transitionend", done, false);
+                    }}
+                    classNames="fade"
+                  >
+                    <div className="button-container">
+                      <Button onClick={() => setState((state) => !state)}>
+                        {state ? "Hello, world!" : "Goodbye, world!"}
+                      </Button>
+                    </div>
+                  </CSSTransition>
+                </SwitchTransition>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function ReactTransitionGroup() {
+  return (
+    <div className="ReactTransitionGroup">
+      <TransitionComp />
+      <CSSTransitionComp />
+      <SwitchTransitionComp />
+    </div>
   );
 }
 
